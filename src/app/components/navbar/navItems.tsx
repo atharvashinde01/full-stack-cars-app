d6d268b3d8702de49758957e326beb0640be4878
atharvashinde01 <atharva28701@gmail.com>
2022-07-09 23:05:16 +0530

    Started Navbar

---
+++ src/app/components/navbar/navItems.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const ListContainer = styled.ul`
+    ${tw`
+        flex
+        list-none
+    `};
+`;
+
+const NavItem = styled.li`
+`;
+
+export function NavItems() {
+  
+}
